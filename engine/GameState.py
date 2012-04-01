56bcc044d4239ab603d487f70844eb47bc6033cb
David Karesh <davidnk@gmail.com>
2012-03-31 23:06:49 -0400

    GameState

--- engine/GameState.py
+++ engine/GameState.py
@@ -1,5 +1,23 @@
 class GameState:
-    def setup(self, cards, initalDeck, players):
+    def setup(cards, initialDeck, players):
         @staticmethod
-        
-        return state;
+        state = GameState()
+        state.players = players
+        state.pcards = [PlayerCards(deck = initialDeck) for p in players]
+        for c in state.pcards:
+            c.draw(5);
+        state.abcs = [{'actions':0, 'buys':0, 'coins':0} for p in players]
+        state.stacks = cards
+        state.turn = players[0]
+        return state
+    
+    def clone(self):
+        state = GameState()
+        state.players = self.players
+        state.pcards = self.pcards
+        state.abcs = self.abcs
+        state.stacks = self.stacks
+        state.turn = self.turn
+        return state
+    
+    
