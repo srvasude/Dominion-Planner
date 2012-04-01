aec5be030bc8282b9808b18dd58c1dee36bd3781
Srinivas Vasudevan <srvasude@gmail.com>
2012-03-31 20:02:57 -0400

    Changed README

--- Cards/card.py
+++
@@ -1,9 +0,0 @@
-def Card(Object):
-    def __init__(self, name='None', coins=0, victoryPoints=0):
-        self.name = name
-        self.coins = coins
-        self.victoryPoints = victoryPoints
-    def doAction(gameState, **kwargs):
-        raise UnsupportedException() 
-    def heuristic(gameState):
-        raise UnsupportedException()
