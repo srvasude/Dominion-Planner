d411043e2e5e368e3e62a0047705d43b2ea10497
David Karesh <davidnk@gmail.com>
2012-04-01 00:47:52 -0400

    Player

---
+++ player/Player.py
@@ -0,0 +1,16 @@
+class Player:
+    def selectInput(self, inputs, gameState, actionSimulator):
+        return NotImplemented()
+    
+    def playActionPhase(self, gameState):
+        NotImplemented()
+    
+    def playBuyPhase(self, gameState):
+        NotImplemented()
+    
+    def valueCard(self, gameState, card):
+        NotImplemented()
+    
+    
+        
+    
