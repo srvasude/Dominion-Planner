2338929be67beb14c09104e3951ae70cb359d5b6
Srinivas Vasudevan <srvasude@gmail.com>
2012-04-27 02:32:12 -0400

    Stupid Karesh

--- player/Player.py
+++ player/Player.py
@@ -1,3 +1,8 @@
+import sys
+import os.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 
+        os.path.pardir)))
+from engine.InputSets import InputSets
 class Player:
     def selectInput(self, inputs, gameState, actionSimulator=None,
             helpMessage=None):
