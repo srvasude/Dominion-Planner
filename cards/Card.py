62571c5ab9c7cd1d273012138d15bd9896fe6d88
David Karesh <davidnk@gmail.com>
2012-04-27 00:52:59 -0400

    Fixed indentation.  Added forgotten keyword.

--- cards/Card.py
+++ cards/Card.py
@@ -1,25 +1,25 @@
 class Card(object):
     def __init__(self, name='None', cost=0, coins=0, victoryPoints=0,
-            action = None, reaction = None, ctype=None):
+        action = None, reaction = None, ctype=None):
         super(object, self).__setattr__('name', name)
         super(object, self).__setattr__('cost', cost)
         super(object, self).__setattr__('coins', coins)
         super(object, self).__setattr__('victoryPoints', victoryPoints)
         super(object, self).__setattr__('action', action)
         super(object, self).__setattr__('reaction', reaction)
-     def __hash__(self):
-         hsh = sum(hash(self.__getattr__(k)) for k in __slots__)
-         return hsh
-     def __setattr__(*args):
+    def __hash__(self):
+        hsh = sum(hash(self.__getattr__(k)) for k in __slots__)
+        return hsh
+    def __setattr__(*args):
         raise TypeError("Can't change immutable class")
-     __delattr__ = __setattr__
-     __slots__ = ('name', 'cost', 'coins', 'victoryPoints', 'action',
-             'reaction')
+    __delattr__ = __setattr__
+    __slots__ = ('name', 'cost', 'coins', 'victoryPoints', 'action',
+         'reaction')
      
 def singleton(cls):
     instances = {}
     def getinstance():
-        if cls not instances:
+        if cls not in instances:
             instances[cls] = cls()
         return instances[cls]
     return getinstance
