0a149d0668adb10e1b16de630ff7856f78da7fb7
Srinivas Vasudevan <srvasude@gmail.com>
2012-04-27 00:31:38 -0400

    Added a Human Player so that one can test

---
+++ player/HumanPlayer.py
@@ -0,0 +1,75 @@
+from Player import Player
+from sys import stdin
+_magicString = 'Choose an option, p being print statistics and hand'
+_noop = ', with -1 being none of the above'
+class HumanPlayer(Player):
+    def _iterateAndAsk(lst, noop=False):
+        for item in enumerate(lst):
+            print item
+        option = None
+        if noop:
+            option = input(_magicString+_noop+':')
+        else:
+            option = input(_magicString+':')
+        return option
+
+    def selectInput(self, inputs, gameState, actionSimulator=None,
+            helpMessage=None):
+        print helpMessage
+        option = _iterateAndAsk(inputs)
+        if option == 'p':
+            print gameState.abcs[gameState.turn], 
+                gameState.pcards[gameState.turn].hand
+        else:
+            option = int(option)
+            return inputs[option]
+
+    def playActionPhase(self, gameState):
+        gameState = gameState.clone()
+        pcards = gameState.pcards[gameState.turn]
+        abcs = gameState.abcs[gameState.turn]
+        cards = self.availableActions(gameState)
+        while cards:
+            option = _iterateAndAsk(cards, True)
+            if option == '-1':
+                break
+            else if option == 'p':
+                print abcs, pcards.hand
+                continue
+            else:
+                option = int(option)
+                gameState.pcards[gameState.turn].discardFromHand(cards[i])
+                gameState.abcs[gameState.turn]['actions'] -= 1
+                gameState = cards[i].action(gameState)
+                cards[i] = availableActions(self, gameState)
+        return gameState
+
+    def playBuyPhase(self, gameState):
+        gameState = gameState.clone()
+        pcards = gameState.pcards[gameState.turn]
+        abcs = gameState.abcs[gameState.turn]
+        buys = gameState.abcs[gameState.turn]['buys']
+        coins = gameState.abcs[gameState.turn]['coins'] +
+            totalTreasure(gameState)
+        while buys > 0:
+            possibleBuys = self.availableBuys(gameState, coins)
+            option = _iterateAndAsk(possibleBuys, True)
+            if option == '-1':
+                break
+            else if option == 'p':
+                print abcs, pcards.hand
+                continue
+            else:
+                option = int(option)
+                gameState.stacks[possibleBuys[i]] -= 1
+                buys -= 1
+                coins -= possibleBuys[i].cost
+                pcards.gain(possibleBuys[i])
+        gameState.abcs[gameState.turn]['buys'] = buys
+        gameState.abcs[gameState.turn]['coins'] = coins
+        return gameState
+
+    def playDiscardPhase(self, gameState):
+        gameState = gameState.clone()
+        gameState.pcards[gameState.turn].discardPhase()
+        return gameState
