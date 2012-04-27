66ee015d487acd6293f6dc5bd8ab1137e66638f4
Srinivas Vasudevan <srvasude@gmail.com>
2012-04-27 00:38:23 -0400

    Must use object rather than Object

--- cards/Card.py
+++ cards/Card.py
@@ -1,4 +1,4 @@
-class Card(Object):
+class Card(object):
     def __init__(self, name='None', cost=0, coins=0, victoryPoints=0,
             action = None, reaction = None, ctype=None):
         super(Object, self).__setattr__('name', name)
