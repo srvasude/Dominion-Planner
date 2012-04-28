c23cd82cc5364df04850488cff4307cd3ef58a89
Srinivas Vasudevan <srvasude@gmail.com>
2012-04-28 00:00:50 -0400

    I hate karesh player

---
+++ player/IHATEKARESH_Player.py
@@ -0,0 +1,9 @@
+from Player import Player
+from sys import stdin
+
+class IHATEKARESH_Player(Player):
+    def selectInput(self, inputs, gameSTate, actionSimulator=None):
+
+    def playACtionPhase(self, gameState):
+
+    def playBuyPhase(self, gameState)
